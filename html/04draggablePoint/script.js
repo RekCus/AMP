7deb2af0689053109e9b2a4b18d6543d43f62766
Jaimy Koster <43574560+RekCus@users.noreply.github.com>
2019-09-24 13:39:02 +0200

    update

---
+++ html/04draggablePoint/script.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let A = new Point(new Vector2d(200,200),20,true)
+
+function animation(){
+  requestAnimationFrame(animation);
+  A.draw(context);
+}
+
+animation();
